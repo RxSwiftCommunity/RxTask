8dc46b6528e6983e35247c9e60f62dd993a385c4
Scott Hoyt <scottrhoyt@gmail.com>
2017-02-19 20:25:31 -0800

    Try new method for testing by dynamically creating temporary scripts.

--- Tests/RxRunnerTests/RxRunnerTests.swift
+++ Tests/RxRunnerTests/RxRunnerTests.swift
@@ -4,20 +4,60 @@ import RxSwift
 import RxBlocking
 
 class RxRunnerTests: XCTestCase {
-    func testExample() throws {
-        let events = try Task(launchPath: "/bin/echo", arguments: ["hello", "world"]).launch()
+    func testStdOut() throws {
+        let script = try createScript(commands: [
+                "echo hello world",
+                "sleep 0.1"
+            ])
+
+        let events = try Task.init(launchPath: script.path).launch()
             .toBlocking()
             .toArray()
 
         XCTAssertEqual(events.count, 3)
-        XCTAssertEqual(events[0], .start(command: "/bin/echo hello world"))
+        XCTAssertEqual(events[0], .start(command: script.path))
         XCTAssertEqual(events[1], .stdOut("hello world\n"))
         XCTAssertEqual(events[2], .exit(statusCode: 0))
+
+        try FileManager.default.removeItem(at: script)
+    }
+
+    func testStdErr() throws {
+        let script = try createScript(commands: [
+            "echo hello world 1>&2",
+            "sleep 0.1"
+            ])
+
+        let events = try Task.init(launchPath: script.path).launch()
+            .toBlocking()
+            .toArray()
+
+        XCTAssertEqual(events.count, 3)
+        XCTAssertEqual(events[0], .start(command: script.path))
+        XCTAssertEqual(events[1], .stdErr("hello world\n"))
+        XCTAssertEqual(events[2], .exit(statusCode: 0))
+
+        try FileManager.default.removeItem(at: script)
     }
 
     static var allTests : [(String, (RxRunnerTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testStdOut", testStdOut),
+            ("testStdErr", testStdErr)
         ]
+    }
+
+    func createScript(commands: [String]) throws -> URL {
+        let fileName = UUID().uuidString + ".sh"
+        let fileURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true).appendingPathComponent(fileName)
+
+        let shebang = "#!/bin/bash"
+        let contents = ([shebang] + commands).joined(separator: "\n")
+        try contents.write(to: fileURL, atomically: true, encoding: .utf8)
+
+        let permissions = Int16(0o0770)
+        try FileManager.default.setAttributes([.posixPermissions: permissions], ofItemAtPath: fileURL.path)
+
+        return fileURL
     }
 }
