2c3201e3567b3384d577f8e521ca26b4cdcc3b38
Scott Hoyt <scottrhoyt@gmail.com>
2017-02-19 22:18:57 -0800

    Extract script creation out to class so that script file lifetime is tied to the class.

--- Tests/RxRunnerTests/RxRunnerTests.swift
+++ Tests/RxRunnerTests/RxRunnerTests.swift
@@ -3,9 +3,34 @@ import XCTest
 import RxSwift
 import RxBlocking
 
+class ScriptFile {
+    private let url: URL
+
+    var path: String {
+        return url.path
+    }
+
+    init(commands: [String]) throws {
+        let fileName = UUID().uuidString + ".sh"
+        let fileURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true).appendingPathComponent(fileName)
+
+        let shebang = "#!/bin/bash"
+        let contents = ([shebang] + commands).joined(separator: "\n")
+        try contents.write(to: fileURL, atomically: true, encoding: .utf8)
+
+        let permissions = Int16(0o0770)
+        try FileManager.default.setAttributes([.posixPermissions: permissions], ofItemAtPath: fileURL.path)
+        url = fileURL
+    }
+
+    deinit {
+        try? FileManager.default.removeItem(at: url)
+    }
+}
+
 class RxRunnerTests: XCTestCase {
     func testStdOut() throws {
-        let script = try createScript(commands: [
+        let script = try ScriptFile(commands: [
                 "echo hello world",
                 "sleep 0.1"
             ])
@@ -18,12 +43,10 @@ class RxRunnerTests: XCTestCase {
         XCTAssertEqual(events[0], .start(command: script.path))
         XCTAssertEqual(events[1], .stdOut("hello world\n"))
         XCTAssertEqual(events[2], .exit(statusCode: 0))
-
-        try FileManager.default.removeItem(at: script)
     }
 
     func testStdErr() throws {
-        let script = try createScript(commands: [
+        let script = try ScriptFile(commands: [
             "echo hello world 1>&2",
             "sleep 0.1"
             ])
@@ -36,8 +59,6 @@ class RxRunnerTests: XCTestCase {
         XCTAssertEqual(events[0], .start(command: script.path))
         XCTAssertEqual(events[1], .stdErr("hello world\n"))
         XCTAssertEqual(events[2], .exit(statusCode: 0))
-
-        try FileManager.default.removeItem(at: script)
     }
 
     static var allTests : [(String, (RxRunnerTests) -> () throws -> Void)] {
@@ -45,19 +66,5 @@ class RxRunnerTests: XCTestCase {
             ("testStdOut", testStdOut),
             ("testStdErr", testStdErr)
         ]
-    }
-
-    func createScript(commands: [String]) throws -> URL {
-        let fileName = UUID().uuidString + ".sh"
-        let fileURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true).appendingPathComponent(fileName)
-
-        let shebang = "#!/bin/bash"
-        let contents = ([shebang] + commands).joined(separator: "\n")
-        try contents.write(to: fileURL, atomically: true, encoding: .utf8)
-
-        let permissions = Int16(0o0770)
-        try FileManager.default.setAttributes([.posixPermissions: permissions], ofItemAtPath: fileURL.path)
-
-        return fileURL
     }
 }
