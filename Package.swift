ccc9a81c2787799d43a73acf666d6998251cba31
Scott Hoyt <scottrhoyt@gmail.com>
2017-02-21 16:35:27 -0800

    Update SPM package for RxSwift version.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "RxTask",
     dependencies: [
-        .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3)
+        .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3, minorVersion: 2)
     ]
 )
