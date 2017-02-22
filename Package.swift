6fe38fb985bc923f99a5bda96a7f164f4304f184
Scott Hoyt <scottrhoyt@gmail.com>
2017-02-21 18:42:35 -0800

    Fix Package.swift.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "RxTask",
     dependencies: [
-        .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3, minorVersion: 2)
+        .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3, minor: 2)
     ]
 )
